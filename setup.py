0ebdfbd052ccc304497751674e7bedb85e438c1d
Xenxia <xen.devops@gmail.com>
2022-05-22 21:06:05 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='TemplateStrPython',
+    packages=find_packages(),
+    version='0.0.0',
+    description='TemplateStr allows to add variable, function, condition and switch in a string',
+    author='Xenxia',
+    license='MIT',
+)
